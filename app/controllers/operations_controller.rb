de9ad84603b06dcc87b7fff3bdcc793cf6e59cee
diegosierra01 <diego.sierra01@uptc.edu.co>
2019-11-08 07:46:46 -0500

    Definición del controlador con las operaciones con el número

--- app/controllers/operations_controller.rb
+++ app/controllers/operations_controller.rb
@@ -3,10 +3,43 @@ class OperationsController < ApplicationController
         @title = 'Tabla de multiplicar'
     end
     def operations
-        @number = params[:number][:title]
-        render plain: params[:number][:title].inspect 
-        puts @number 
+        @number = params[:number][:title].to_i
+        @factorial = factorial
+        @tabla = tabla
+        if primo > 2
+            @primo = 'no es primo'
+        else
+            @primo = 'es primo'
+        end
+        puts @factorial
     end
     def factorial
+        i = 1
+        answer = 1
+        while i <= @number  do
+            i += 1
+            answer *= i
+        end
+        return answer
+    end
+    def primo
+        i = 1
+        divider = 0
+        while i <= @number  do
+            if @number % i == 0
+                divider += 1
+            end
+            i += 1
+        end
+        return divider
+    end
+    def tabla
+        i = 1
+        objArray = Array.new
+        while i < 10 do
+           objArray.push (@number).to_s + '*' + (i).to_s + '=' + (i*@number).to_s
+           i += 1
+        end
+        return objArray
     end
 end
